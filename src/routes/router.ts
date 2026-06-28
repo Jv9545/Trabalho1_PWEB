b773dda31d0ca8c03d24ff18b88834c6bf7cb1f8
Jv9545 <josevinicius9545@gmail.com>
2026-06-28 15:45:16 -0300

    router.ts criado e endpoits Carros definidos

---
+++ src/routes/router.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from 'express';
+import { CarroController } from '../controllers/carroController';
+
+const router = Router();
+const carroController = new CarroController();
+
+// --- Rotas Carros ---
+router.get('/carros/disponiveis', (req: Request, res: Response) => { carroController.exibirCarrosDisponiveis(req, res); });
+router.get('/carros', (req: Request, res: Response) => { carroController.exibirCarros(req, res); });
+router.post('/carros', (req: Request, res: Response) => { carroController.cadastrarCarros(req, res); });
+router.get('/carros/:id', (req: Request, res: Response) => { carroController.exibirCarroID(req, res); });
+router.put('/carros/:id', (req: Request, res: Response) => { carroController.atualizarCarro(req, res); });
+router.delete('/carros/:id', (req: Request, res: Response) => { carroController.removerCarro(req, res); });
+
+export default router;
